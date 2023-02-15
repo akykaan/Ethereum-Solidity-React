ad2ff9da80c3a0d715778c393f0b25f171eb3b62
akykaan <kaanaky77@gmail.com>
2023-02-15 22:34:23 +0300

    server.js and routes.js added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63e62ff0d08787d6"; }
+/******/ 		__webpack_require__.h = function() { return "8ff3925c64aa093a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
